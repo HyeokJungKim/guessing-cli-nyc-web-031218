0c347cd17abc58727693bd2260032539c2549eaf
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-06 16:59:08 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,14 +4,17 @@ def run_guessing_game
   i = rand(6) + 1
   input = gets.chomp
   num = input.to_i
-  until (num == i || input == "exit")
-    puts "The computer guessed #{i}."
-    input = gets.chomp
-    num = input.to_i
-  end
+
   if input == "exit"
     puts "Goodbye!"
   elsif num == i 
     puts "You guessed the correct number!"
   end
+
+  # until (num == i || input == "exit")
+  #   puts "The computer guessed #{i}."
+  #   input = gets.chomp
+  #   num = input.to_i
+  # end
+  
 end
